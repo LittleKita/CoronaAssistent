330da949be2eb344f24cc7bf4627d78ae02bae8b
LittleKita <clange-wvv@nw7.de>
2020-03-22 09:27:02 +0100

    add frontend to backent text2speech

---
+++ frontend/src/app/share/text-to-speech.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class TextToSpeechService {
+
+  constructor() {
+  }
+
+  public synthesizeSpeech(text: string, callback: (content: Blob) => void): void {
+    let url: string;
+    if (location.href.indexOf('localhost') >= 0 || location.href.indexOf('127.0.0.1') >= 0) {
+      url = 'ws://localhost:12345/t2s';
+    } else {
+      if (location.protocol === 'https:') {
+        url = 'wss:';
+      } else {
+        url = 'ws:';
+      }
+      url += '//' + location.host;
+      url += location.pathname + '/t2s';
+    }
+    const webSocket: WebSocket = new WebSocket(url);
+    webSocket.onopen = () => {
+      webSocket.send(text);
+    };
+    webSocket.onmessage = (msg) => {
+      console.log('message', msg.data);
+      callback(msg.data);
+    };
+    webSocket.onclose = () => {
+      console.log('close');
+    };
+    webSocket.onerror = (error) => {
+      console.error(error);
+    };
+  }
+}
--- frontend/src/app/start/start.component.ts
+++ frontend/src/app/start/start.component.ts
@@ -1,27 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { TextToSpeechService } from '../share/text-to-speech.service';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
-export class StartComponent implements OnInit {
+export class StartComponent implements OnInit, AfterViewInit {
 
   output = '';
   running = false;
   stream: MediaStream;
 
-  constructor() { }
+  @ViewChild('audio', { read: ElementRef, static: false })
+  audio: ElementRef<HTMLAudioElement>;
+
+  constructor(private textToSpeechService: TextToSpeechService) {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    window.setTimeout(() => {
+      this.appendOutput('Hallo ich bin der Corona-Assitent. Wie kann ich dir helfen?');
+    }, 100);
+  }
+
+  private appendOutput(text: string, callback?: () => void): void {
+    this.textToSpeechService.synthesizeSpeech(text, (data: Blob) => {
+      this.audio.nativeElement.src = window.URL.createObjectURL(data);
+      this.audio.nativeElement.play();
+    });
+    this.output += text + '\r\n';
+  }
+
   startRecordning(): void {
-    if(this.running) {
+    if (this.running) {
       this.running = false;
-      this.output += 'Die Verbindung zum Microphone wurde getrennt.\r\n';
-      this.stream.getTracks().forEach((track: MediaStreamTrack) => {
-        track.stop();
+      this.appendOutput('Die Verbindung zum Microphone wurde getrennt.', () => {
+        this.stream.getTracks().forEach((track: MediaStreamTrack) => {
+          track.stop();
+        });
       });
       return;
     }
@@ -58,11 +78,18 @@ export class StartComponent implements OnInit {
       }, (stream: MediaStream) => {
         this.stream = stream;
         this.running = true;
-        this.output += 'Das Microphone wurde erfolgreich verbunden.\r\n';
-        this.output += 'Du kannst jetzt Sprechen.\r\n';
+
         console.log(stream);
+
+        this.appendOutput('Das Microphone wurde erfolgreich verbunden.', () => {
+          this.appendOutput('Du kannst jetzt Sprechen.', () => {
+            console.log('OK');
+          });
+        });
       }, (error: MediaStreamError) => {
-        this.output += 'Das Microphone konnte leider nicht verwendet werden: "' + error + '"\r\n';
+        this.appendOutput('Das Microphone konnte leider nicht verbunden werden.', () => {
+        });
+        this.output += 'Fehler: "' + error + '"\r\n';
         console.log(error);
       });
   }
